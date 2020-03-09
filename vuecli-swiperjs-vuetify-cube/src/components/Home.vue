c0daaf63dfab13ef26f9cbb7b0732d9ade84bb77
Mario Rosan <54136861+mariorosan@users.noreply.github.com>
2020-03-09 12:11:04 +0100

    ...

--- vuecli-swiperjs-vuetify-cube/src/components/Home.vue
+++ vuecli-swiperjs-vuetify-cube/src/components/Home.vue
@@ -5,6 +5,10 @@
         <h1 class="headline">Mobile Swiper || Home</h1>
       </v-col>
       <v-col cols="11">
+        <div class="logo">
+          <h1>SwiperJS</h1>
+          <img src="../assets/logo.png" alt="">
+        </div>
       </v-col>
     </v-row>
   </div>
@@ -22,5 +26,16 @@ export default {
     font-size: 50px;
     color: #ffffff;
     margin-top: 30px;
+  }
+  .logo {
+   margin-left: 60%;
+   margin-top: 100%;
+  }
+  .logo h1 {
+    font-size: 30px;
+    color: #ffffff;
+  }
+  img {
+    width: 120px;
   }
 </style>
